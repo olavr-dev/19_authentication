a4b44ca7b9a3e71c6805c1756b0eacaeb4ee5aad
Olav Rorvik <olavr.dev@gmail.com>
2024-04-18 21:37:00 +0200

    added cookie auth logic

--- routes/demo.js
+++ routes/demo.js
@@ -73,12 +73,18 @@ router.post('/login', async function (req, res) {
     return res.redirect('/login');
   }
 
-  console.log('User is authenticated!');
-  res.redirect('/admin');
+  req.session.user = { id: existingUser._id, email: existingUser.email };
+  req.session.isAuthenticated = true;
+  req.session.save(function () {
+    res.redirect('/admin');
+  });
 });
 
 router.get('/admin', function (req, res) {
-  // Check the users "ticket"
+  if (!req.session.isAuthenticated) {
+    console.log(req.session);
+    return res.status(401).render('401');
+  }
   res.render('admin');
 });
 
